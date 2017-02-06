8369646bc299b3c264888522e0632030266100a1
Ahtesham Ul Haq(@hptouchpad519) <hptouchpad519@gmail.com>
2017-02-06 14:50:54 +0500

    Create Hello-world.cpp

---
+++ Hello-world.cpp
@@ -0,0 +1,9 @@
+// comments.cpp
+//demonstrates comments
+#include <iostream> //Preprocessor directive
+using namespace std; //using  directive.
+int main()      // function name "main"
+{               //start of the function
+cout<< "Hello World\n";   //statement
+return 0;        // statement
+}   //function close.
